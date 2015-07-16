e8b59c04411095346077d936ebe612e94ad0a094
Regis128 <regis1286@gmail.com>
2015-07-16 10:10:55 +0200

    URLs fixed

--- src/main/java/energymeter/controllers/EnergyController.java
+++ src/main/java/energymeter/controllers/EnergyController.java
@@ -12,8 +12,11 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Locale;
 
 
 @RestController
@@ -28,9 +31,14 @@ public class EnergyController {
     @RequestMapping(value="energy/data/{type}", method=RequestMethod.GET)
     public ArrayList<EnergyAbstract> getAll(@PathVariable(value="type") String type,
                          @RequestParam(value = "limit", required=false) Integer limit) throws Exception {
-        EnergyTypesEnum typeEnum = EnergyTypesEnum.valueOf(type.toUpperCase());
-        ArrayList<EnergyAbstract> energyAbstractPortions = energyDAO.getAllEnergy(typeEnum, limit);
-        return energyAbstractPortions;
+        try {
+            EnergyTypesEnum typeEnum = EnergyTypesEnum.valueOf(type.toUpperCase());
+            ArrayList<EnergyAbstract> energyAbstractPortions = energyDAO.getAllEnergy(typeEnum, limit);
+            return energyAbstractPortions;
+        }
+        catch(Exception e) {
+            return new ArrayList<>();
+        }
     }
 
     @RequestMapping("energy/data/{type}/{deviceID}")
@@ -38,32 +46,52 @@ public class EnergyController {
     ArrayList<EnergyAbstract> getByDevice(@PathVariable(value="type") String type,
                        @PathVariable(value="deviceID") String deviceID,
                        @RequestParam(value = "limit", required=false) Integer limit) throws Exception {
-        EnergyTypesEnum typeEnum = EnergyTypesEnum.valueOf(type.toUpperCase());
-        ArrayList<EnergyAbstract> energies = energyDAO.getEnergyById(typeEnum, deviceID, limit);
-        return energies;
+        try {
+            EnergyTypesEnum typeEnum = EnergyTypesEnum.valueOf(type.toUpperCase());
+            ArrayList<EnergyAbstract> energies = energyDAO.getEnergyById(typeEnum, deviceID, limit);
+            return energies;
+        }
+        catch(Exception e) {
+            return new ArrayList<>();
+        }
     }
 
     @RequestMapping("energy/data/{type}/{deviceID}/{date}")
     public @ResponseBody
     ArrayList<EnergyAbstract> getByDeviceDate(@PathVariable(value="type") String type,
                            @PathVariable(value="deviceID") String deviceID,
-                           @PathVariable(value="date") @DateTimeFormat(iso= DateTimeFormat.ISO.DATE) Date date,
+                           @PathVariable(value="date") String dateStr,
                            @RequestParam(value = "limit", required=false) Integer limit) throws Exception {
-        EnergyTypesEnum typeEnum = EnergyTypesEnum.valueOf(type.toUpperCase());
-        ArrayList<EnergyAbstract> energies = energyDAO.getEnergyByIdDate(typeEnum, deviceID, date, limit);
-        return energies;
+        try {
+            DateFormat format = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH);
+            Date date = format.parse(dateStr);
+            EnergyTypesEnum typeEnum = EnergyTypesEnum.valueOf(type.toUpperCase());
+            ArrayList<EnergyAbstract> energies = energyDAO.getEnergyByIdDate(typeEnum, deviceID, date, limit);
+            return energies;
+        }
+        catch(Exception e) {
+            return new ArrayList<>();
+        }
     }
 
-    @RequestMapping("energy/data/{type}/{deviceID}/{datefrom}/{dateto}")
+    @RequestMapping("energy/data/{type}/{deviceID}/{dateFrom}/{dateTo}")
     public @ResponseBody
     ArrayList<EnergyAbstract> getByDeviceDates(@PathVariable(value="type") String type,
                                               @PathVariable(value="deviceID") String deviceID,
-                                              @PathVariable(value="datefrom") @DateTimeFormat(iso= DateTimeFormat.ISO.DATE) Date datefrom,
-                                               @PathVariable(value="dateto") @DateTimeFormat(iso= DateTimeFormat.ISO.DATE) Date dateto,
+                                              @PathVariable(value="dateFrom") @DateTimeFormat(iso= DateTimeFormat.ISO.DATE) String dateFromStr,
+                                               @PathVariable(value="dateTo") @DateTimeFormat(iso= DateTimeFormat.ISO.DATE) String dateToStr,
                                               @RequestParam(value = "limit", required=false) Integer limit) throws Exception {
-        EnergyTypesEnum typeEnum = EnergyTypesEnum.valueOf(type.toUpperCase());
-        ArrayList<EnergyAbstract> energies = energyDAO.getEnergyByIdDates(typeEnum, deviceID, datefrom, dateto, limit);
-        return energies;
+        try {
+            DateFormat format = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH);
+            Date dateFrom = format.parse(dateFromStr);
+            Date dateTo = format.parse(dateToStr);
+            EnergyTypesEnum typeEnum = EnergyTypesEnum.valueOf(type.toUpperCase());
+            ArrayList<EnergyAbstract> energies = energyDAO.getEnergyByIdDates(typeEnum, deviceID, dateFrom, dateTo, limit);
+            return energies;
+        }
+        catch(Exception e) {
+            return new ArrayList<>();
+        }
     }
 
     @RequestMapping("meters")
@@ -85,7 +113,12 @@ public class EnergyController {
     ArrayList<EnergyAbstract> getPredictions(@PathVariable(value="deviceID") String deviceID,
                                              @RequestParam(value="days", required=false) Integer days,
                                              @RequestParam(value="limit", required=false) Integer limit) throws Exception {
-        ArrayList<EnergyAbstract> predictions = predictionDAO.predict(deviceID, days, limit);
-        return predictions;
+        try {
+            ArrayList<EnergyAbstract> predictions = predictionDAO.predict(deviceID, days, limit);
+            return predictions;
+        }
+        catch(Exception e) {
+            return new ArrayList<>();
+        }
     }
 }
