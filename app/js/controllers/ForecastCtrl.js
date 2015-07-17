53d9841ea4408ed683014f798b3caa66472747fa
Wojciech A <wojciech.adaszynski@gmail.com>
2015-07-17 16:08:26 +0200

    Directive

--- app/js/controllers/ForecastCtrl.js
+++ app/js/controllers/ForecastCtrl.js
@@ -7,11 +7,6 @@ app.controller("ForecastCtrl", ['$scope', '$http','WeatherService', 'ChartFactor
         {name: "pressure", value:"1"}
     ];
     $scope.forecastOption = $scope.forecastOptions[0];
-    $scope.chartData = ChartFactory.getChartConfiguration({
-       domain: [],
-        label: 'Temperature',
-        data: []
-    });
     $scope.pressureChart = ChartFactory.getChartConfiguration({
         domain: [],
         label: 'Pressure',
@@ -29,37 +24,45 @@ app.controller("ForecastCtrl", ['$scope', '$http','WeatherService', 'ChartFactor
       humidity: [],
       pressure: []
     };
+    $scope.config = {
+        title: {
+            text: "Fruits sold in First Quarter"
+        },
+        data: [
+            {
+                type: "line",
+                dataPoints: []
+            },
+            {
+                type: "line",
+                axisYType: "secondary",
+                dataPoints: []
+            }
+        ]
+    };
+
     $scope.updateForecast = function(data){
-        var domain = [];
-        $scope.dataSets.temperature = [];
-        $scope.dataSets.domain = [];
-        $scope.dataSets.temperature = [];
-        $scope.dataSets.humidity = [];
+        $scope.config.data[0].dataPoints = [];
+        $scope.config.data[1].dataPoints = [];
+
         for (var i = 0; i < data.list.length; i++){
-            domain.push(new Date(data.list[i].dt * 1000).toLocaleDateString());
-            $scope.dataSets.domain.push(new Date(data.list[i].dt * 1000).toLocaleDateString());
-            $scope.dataSets.temperature.push(data.list[i].temp.day);
-            $scope.dataSets.humidity.push(data.list[i].humidity);
-            $scope.dataSets.pressure.push(data.list[i].pressure);
+            $scope.config.data[0].dataPoints.push({
+                x: new Date(data.list[i].dt * 1000),
+                y: data.list[i].temp.day
+            });
+            $scope.config.data[1].dataPoints.push({
+                x: new Date(data.list[i].dt * 1000),
+                y: data.list[i].pressure
+            });
         }
-    };
-
-    $scope.chartOptions = {
-        responsive : true
-    };
-
-    $scope.chartUpdate = function () {
-        $scope.chartData.labels = $scope.dataSets.domain;
-        $scope.chartData.datasets[0].data = $scope.dataSets[$scope.forecastOption.name];
+        console.log($scope.config.data);
     };
 
     $scope.getForecast = function(){
         WeatherService.getLongForecast($scope.days)
             .then(function (data) {
                 $scope.updateForecast(data.data);
-                $scope.chartUpdate();
             });
     };
     $scope.getForecast();
-
 }]);
