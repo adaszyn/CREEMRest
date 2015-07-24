f8999f29737e8a0918c82e41586669ed6dc9729e
Wojciech A <wojciech.adaszynski@gmail.com>
2015-07-24 15:41:10 +0200

    sample forecast test

--- app/js/test/ForecastTest.js
+++ app/js/test/ForecastTest.js
@@ -27,19 +27,26 @@ describe("Forecast page", function() {
 
     it('Valid DateRange changed on daysRange change', function () {
         scope.daysRange.to = 0;
-        expect(scope.dateRange.to.getDate()).toEqual((new Date()).getDate());
-    });
-
-    it('Invalid DateRange changed on daysRange change', function () {
-        $httpBackend.whenGET(/http:\/\/.*/).respond(function () {
+        scope.daysRange.from = -2;
+        $httpBackend.whenGET(/http:\/\/api\.openweathermap\.org\/data\/2\.5\/.*/).respond(function () {
             return [200, ['success'], {}];
         });
-        $httpBackend.whenGET(/http:\/\/.*/).respond(function () {
-            return [200, ['success'], {}];
-        });
-        scope.daysRange.to = 5;
         scope.$digest();
         $httpBackend.flush();
-        expect(scope.dateRange.to.getDate()).not.toBe((new Date()).getDate());
+        expect(scope.dateRange.to.getDate()).toEqual((new Date()).getDate());
+        expect(scope.dateRange.from.getDate()).toEqual((new Date()).getDate() - 2);
     });
+
+    //it('Invalid DateRange changed on daysRange change', function () {
+    //    $httpBackend.whenGET(/http:\/\/.*/).respond(function () {
+    //        return [200, ['success'], {}];
+    //    });
+    //    $httpBackend.whenGET(/http:\/\/.*/).respond(function () {
+    //        return [200, ['success'], {}];
+    //    });
+    //    scope.daysRange.to = 5;
+    //    scope.$digest();
+    //    $httpBackend.flush();
+    //    expect(scope.dateRange.to.getDate()).not.toBe((new Date()).getDate());
+    //});
 });
